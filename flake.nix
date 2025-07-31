8134a01e5a14581e9feb196fe665de2e49c31fe3
Declarative_Technologies <declarative-technologies@outlook.com>
2025-07-31 01:19:53 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,63 +5,61 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs   = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, … }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs   = import nixpkgs { inherit system; };
 
-      # 1) define the package
-      remoteMouseDrv = pkgs.stdenv.mkDerivation rec {
-        pname    = "remotemouse";
-        version  = "2023-01-25";
-        src      = pkgs.fetchzip {
-          url       = "https://www.remotemouse.net/downloads/linux/RemoteMouse_x86_64.zip";
-          hash      = "sha256-kmASvBKJW9Q1Z7ivcuKpZTBZA9LDWaHQerqMcm+tai4=";
-          stripRoot = false;
-        };
-
-        nativeBuildInputs = [ pkgs.makeWrapper pkgs.patchelf ];
-        dontPatchELF     = true;
-        dontStrip        = true;
-
-        installPhase = ''
-          mkdir -p $out/opt/remotemouse
-          cp -r RemoteMouse lib images $out/opt/remotemouse/
-          # …your wrapper + patchelf bits here…
-        '';
-
-        meta = with pkgs.lib; {
-          description = "Remote Mouse proprietary binary for NixOS";
-          license     = licenses.unfreeRedistributable;
-          platforms   = [ "x86_64-linux" ];
-        };
+    # 1) define the package
+    remoteMouseDrv = pkgs.stdenv.mkDerivation rec {
+      pname    = "remotemouse";
+      version  = "2023-01-25";
+      src      = pkgs.fetchzip {
+        url       = "https://www.remotemouse.net/downloads/linux/RemoteMouse_x86_64.zip";
+        hash      = "sha256-kmASvBKJW9Q1Z7ivcuKpZTBZA9LDWaHQerqMcm+tai4=";
+        stripRoot = false;
       };
 
-      # 2) overlay so `pkgs.remotemouse` exists
-      overlay = final: prev: {
-        remotemouse = remoteMouseDrv;
-      };
-    in {
-      # make it buildable via `nix build`
-      defaultPackage.${system} = remoteMouseDrv;
+      nativeBuildInputs = [ pkgs.makeWrapper pkgs.patchelf ];
+      dontPatchELF     = true;
+      dontStrip        = true;
 
-      # expose a NixOS module
-      nixosModules.remotemouse = { config, lib, pkgs, ... }: {
-        config = {
-          # pull in our overlay
-          nixpkgs.overlays        = [ overlay ];
+      installPhase = ''
+        mkdir -p $out/opt/remotemouse
+        cp -r RemoteMouse lib images $out/opt/remotemouse/
+        # …your wrapper + patchelf bits here…
+      '';
 
-          # allow fetching the unfree binary
-          nixpkgs.config.allowUnfree = true;
-
-          environment.systemPackages = [
-            pkgs.remotemouse
-            pkgs.xorg.xhost
-          ];
-
-          networking.firewall.allowedTCPPorts = [ 1978 ];
-          networking.firewall.allowedUDPPorts = [ 1978 ];
-        };
+      meta = with pkgs.lib; {
+        description = "Remote Mouse proprietary binary for NixOS";
+        license     = licenses.unfreeRedistributable;
+        platforms   = [ "x86_64-linux" ];
       };
     };
+
+    # 2) overlay so `pkgs.remotemouse` exists
+    overlay = final: prev: {
+      remotemouse = remoteMouseDrv;
+    };
+  in {
+    # make it buildable via `nix build`
+    defaultPackage.${system} = remoteMouseDrv;
+
+    # expose a NixOS module
+    nixosModules.remotemouse = { config, lib, pkgs, … }: {
+      config = {
+        # ← everything goes in here
+        nixpkgs.overlays          = [ overlay ];  
+        nixpkgs.config.allowUnfree = true;
+
+        environment.systemPackages = [
+          pkgs.remotemouse
+          pkgs.xorg.xhost
+        ];
+
+        networking.firewall.allowedTCPPorts = [ 1978 ];
+        networking.firewall.allowedUDPPorts = [ 1978 ];
+      };
+    };
+  };
 }
